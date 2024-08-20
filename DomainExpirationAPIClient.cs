ee9475f2930a5ca1b9a3900fb7177fb47d1108fc
APIVerve Team <hello@apiverve.com>
2024-08-20 10:50:23 -0500

    Bug Fixes

--- DomainExpirationAPIClient.cs
+++ DomainExpirationAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, domainexpirationQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, DomainExpirationQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(domainexpirationQueryOptions options = null)
+        public ResponseObj Execute(DomainExpirationQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(domainexpirationQueryOptions options)
+        private string constructURL(DomainExpirationQueryOptions options)
         {
             string url = _apiEndpoint;
 
