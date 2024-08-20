5ae5d2494346b8282ec2b7fc2663d5f87bb2dc6e
APIVerve Team <hello@apiverve.com>
2024-08-20 10:50:26 -0500

    Bug Fixes

---
+++ DomainExpirationQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DomainExpirationQueryOptions {
+public string domain { get; set; }
+}
+
+}
