f6b49834ccd2fa9aee5cfebc63e252c18bd959b7
APIVerve Team <hello@apiverve.com>
2024-08-20 10:50:29 -0500

    Bug Fixes

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -6,26 +6,26 @@ namespace APIVerve
 {
 public class data
 {
-    [JsonProperty("createdDate")]
-    public DateTime createdDate { get; set; }
-
-    [JsonProperty("daysSinceLastUpdate")]
-    public int daysSinceLastUpdate { get; set; }
-
-    [JsonProperty("daysToExpiration")]
-    public int daysToExpiration { get; set; }
-
     [JsonProperty("domain")]
     public string domain { get; set; }
-
-    [JsonProperty("domainAgeDays")]
-    public int domainAgeDays { get; set; }
 
     [JsonProperty("expirationDate")]
     public DateTime expirationDate { get; set; }
 
+    [JsonProperty("daysToExpiration")]
+    public int daysToExpiration { get; set; }
+
+    [JsonProperty("createdDate")]
+    public DateTime createdDate { get; set; }
+
     [JsonProperty("lastUpdatedDate")]
     public DateTime lastUpdatedDate { get; set; }
+
+    [JsonProperty("daysSinceLastUpdate")]
+    public int daysSinceLastUpdate { get; set; }
+
+    [JsonProperty("domainAgeDays")]
+    public int domainAgeDays { get; set; }
 
 }
 
@@ -39,6 +39,9 @@ public class ResponseObj
 
     [JsonProperty("data")]
     public data data { get; set; }
+
+    [JsonProperty("code")]
+    public int code { get; set; }
 
 }
 
